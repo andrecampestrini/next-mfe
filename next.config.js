178d09e4b32304f8bf9647722271d9a549f2a7d0
André Campestrini <andre.bnu3@gmail.com>
2023-02-14 15:30:03 -0700

    add module federation plugin

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "next-mfe",
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {
+          "./index": "./src/pages/index.ts",
+        },
+        shared: {},
+      })
+    );
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
